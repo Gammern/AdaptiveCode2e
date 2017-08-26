5bcd36d4ef4d248dfa0cb9e7ec4471edc22ec445
Johan Karl Larsen <johan@itlarsen.net>
2017-08-26 20:06:23 +0200

    C# inheritance is invariant. Listing 9-21

---
+++ Chapter09/Variance/SubtypeCovariance/Persitence/EntityRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SubtypeCovariance.Persitence
+{
+    using Model;
+
+    public class EntityRepository
+    {
+        public virtual Entity GetByID(Guid id) => new Entity();
+    }
+}
---
+++ Chapter09/Variance/SubtypeCovariance/Persitence/UserRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SubtypeCovariance.Persitence
+{
+    using Model;
+
+    public class UserRepository : EntityRepository
+    {
+        public override User GetByID(Guid id) => new User(); // Error. C# inheritance is not covariant
+    }
+}
