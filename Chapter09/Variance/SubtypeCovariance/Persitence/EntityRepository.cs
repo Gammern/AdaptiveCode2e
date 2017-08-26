acb1fd29034a743b0f487bc1281a2d7b1ce9bb38
Johan Karl Larsen <johan@itlarsen.net>
2017-08-26 21:41:32 +0200

    generic repository. out param on interface is key!

--- Chapter09/Variance/SubtypeCovariance/Persitence/EntityRepository.cs
+++ Chapter09/Variance/SubtypeCovariance/Persitence/EntityRepository.cs
@@ -4,8 +4,11 @@ namespace SubtypeCovariance.Persitence
 {
     using Model;
 
-    public class EntityRepository : IEntityRepository<Entity>
+    public class EntityRepositoryType<T> : IEntityRepository<T> where T : Entity, new()
     {
-        public virtual Entity GetByID(Guid id) => new Entity();
+        public T GetByID(Guid ID) => new T();
     }
+
+    public class EntityRepository : EntityRepositoryType<Entity> { }
+    public class UserRepository : EntityRepositoryType<User> { }
 }
--- Chapter09/Variance/SubtypeCovariance/Program.cs
+++ Chapter09/Variance/SubtypeCovariance/Program.cs
@@ -10,8 +10,9 @@ namespace SubtypeCovariance
         static void Main(string[] args)
         {
             IEntityRepository<Entity> repo = new UserRepository();
-            var entity = repo.GetByID(Guid.Empty);
+            Entity entity = repo.GetByID(Guid.Empty);
             Console.WriteLine(entity.ToString() + " should be User");
+
             repo = new EntityRepository();
             entity = repo.GetByID(Guid.Empty);
             Console.WriteLine(entity.ToString() + " should be Entity");
