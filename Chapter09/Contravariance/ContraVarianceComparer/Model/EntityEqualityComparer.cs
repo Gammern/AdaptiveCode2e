9b114b3e6ca28376e2a21a942cc1d3955905a7ba
Johan Karl Larsen <johan@itlarsen.net>
2017-08-27 11:55:50 +0200

    Code talks clearly, book doesn't. Look for other sources.

---
+++ Chapter09/Contravariance/ContraVarianceComparer/Model/EntityEqualityComparer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace ContraVarianceComparer.Model
+{
+    public class EntityEqualityComparer: IEqualityComparer<Entity>
+    {
+        public bool Equals(Entity left, Entity right)
+        {
+            return left.ID == right.ID;
+        }
+
+        public int GetHashCode(Entity obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+}
---
+++ Chapter09/Contravariance/ContraVarianceComparer/Model/User.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ContraVarianceComparer.Model
+{
+    public class User : Entity
+    {
+        public string EmailAddress { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
