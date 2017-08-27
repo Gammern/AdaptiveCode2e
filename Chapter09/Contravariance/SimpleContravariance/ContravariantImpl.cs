78d27c50b2575bd5c1e40e6cd50bfc6d56e33e63
Johan Karl Larsen <johan@itlarsen.net>
2017-08-27 10:07:28 +0200

    Contravariance p.285

---
+++ Chapter09/Contravariance/SimpleContravariance/ContravariantImpl.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SimpleContravariance
+{
+    public class SubContravariant : IContravariant<SubType>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="parameter">Will only accept SubType containing Function3()</param>
+        public void MethodWhichAcceptsT(SubType parameter)
+        {
+            Console.WriteLine($"{this.GetType().Name}.MethodWitchAcceptsT({parameter.GetType().Name}) Method3() = {parameter.Method3()}");
+        }
+    }
+
+    public class SuperContravariant : IContravariant<SuperType>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="parameter">Accept SuperType and SubType</param>
+        public void MethodWhichAcceptsT(SuperType parameter)
+        {
+            Console.WriteLine($"{this.GetType().Name}.MethodWitchAcceptsT({parameter.GetType().Name}) Method3() don't exist");
+        }
+    }
+}
---
+++ Chapter09/Contravariance/SimpleContravariance/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SimpleContravariance
+{
+    class Program
+    {
+        static SuperType aSuperType = new SuperType();
+        static SubType aSubType = new SubType();
+
+        static void Main(string[] args)
+        {
+            IContravariant<SuperType> iSuperCopy, iSuper = new SuperContravariant();
+            IContravariant<SubType> iSubCopy, iSub = new SubContravariant();
+
+            iSubCopy = iSuper;    // child = base, relationship reversed
+            // iSuperCopy = iSub; // base = child 
+
+            // iSuper don't rely on SubType.Method3
+            iSuper.MethodWhichAcceptsT(aSubType); 
+            iSuper.MethodWhichAcceptsT(aSuperType); // 
+
+            // iSub rely on SubType.Method3
+            iSub.MethodWhichAcceptsT(aSubType); 
+            // iSub.MethodWhichAcceptsT(aSuperType); // SyperType dont have Method3()
+        }
+    }
+}
