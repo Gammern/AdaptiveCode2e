831c9dc2c317252d5a6a67eac0f206eff5cf021e
Johan Karl Larsen <johan@itlarsen.net>
2017-08-31 18:49:04 +0200

    ISave audit decorator

---
+++ Chapter10/Segregate/Segregate.Audit/AuditInfo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Segregate.Audit
+{
+    public class AuditInfo
+    {
+        public string UserName { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
