b578d0190fff4d4859c1dfb731ad530107f26deb
Johan Karl Larsen <johan@itlarsen.net>
2017-09-02 09:45:53 +0200

    I've had it with chapter 10. Revisit at a later time.

---
+++ Chapter10/Segregate/Segregate.Controllers/GenericController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Segregate.Controllers
+{
+    using Common;
+
+    public class GenericController<TEntity>
+    {
+        private readonly IRead<TEntity> reader;
+        private readonly ISave<TEntity> saver;
+        private readonly IDelete<TEntity> deleter;
+
+        public GenericController(IRead<TEntity> entityReader, ISave<TEntity> entitySaver, IDelete<TEntity> entityDeleter)
+        {
+            this.reader = entityReader;
+            this.saver = entitySaver;
+            this.deleter = entityDeleter;
+        }
+        
+        public void CreateEntity(TEntity entity)
+        {
+            saver.Save(entity);
+        }
+
+        public TEntity GetSingleEntity(Guid identity)
+        {
+            return reader.ReadOne(identity);
+        }
+
+        public void UpdateEntity(TEntity entity)
+        {
+            saver.Save(entity);
+        }
+
+        public void DeleteEntity(TEntity entity)
+        {
+            deleter.Delete(entity);
+        }
+    }
+}
